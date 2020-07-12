3b6c89a4833126245d8ae98315f1a0e783036640
Jelle Breuer <jelle@breuclan.nl>
2020-07-12 20:50:47 +0200

    Added import to config

--- src/configHandler.js
+++ src/configHandler.js
@@ -4,4 +4,12 @@ let file = require('./config.js');
 //     file = require('../../../steasy.config.js');
 // } catch (e) {}
 
+const theme = file.theme;
+
+for (const [name, value] of Object.entries(theme)) {
+  if (typeof value === 'function') {
+    theme[name] = value(theme)
+  }
+}
+
 export default file;
