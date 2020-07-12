4ce5633ad39397c2b9001303f4733ce371f3fc8b
Jelle Breuer <jelle@breuclan.nl>
2020-07-12 20:53:58 +0200

    Added width and height

--- src/transformers/height.js
+++ src/transformers/height.js
@@ -1,4 +1,4 @@
-// import generator from '../generator';
-// // import config from '../configHandler';
+import generator from '../generator';
+import config from '../configHandler';
 
-// export default generator.generate('h', 'height', config.theme.height);
+export default generator.generate('h', 'height', config.theme.height);
--- src/transformers/width.js
+++ src/transformers/width.js
@@ -1,4 +1,4 @@
-// import generator from '../generator';
-// // import config from '../configHandler';
+import generator from '../generator';
+import config from '../configHandler';
 
-// export default generator.generate('w', 'width', config.theme.width);
+export default generator.generate('w', 'width', config.theme.width);
