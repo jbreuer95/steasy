46f96b0f3cd55e7168c5346153e3e2b735f1ba04
Jelle Breuer <jelle@breuclan.nl>
2020-07-16 14:52:31 +0200

    Basic styling to proptable

--- website/.vuepress/components/PropTable.vue
+++ website/.vuepress/components/PropTable.vue
@@ -1,5 +1,15 @@
 <template>
-    <p v-if="methods">{{ JSON.stringify(this.methods) }}</p>
+  <div>
+    <table v-if="Object.keys(this.methods).length > 0">
+      <tr v-for="(value, name) in methods">
+        <td>{{name}}</td>
+        <td>
+          <p style="padding: 0;margin:0" v-for="(v, n) in value">"{{n}}": {{v}}</p>
+        </td>
+      </tr>
+    </table>
+    <h2 v-else>TODO</h2>
+  </div>
 </template>
 <script>
 export default {
@@ -11,7 +21,7 @@ export default {
     },
     data () {
         return {
-            methods: null
+            methods: {}
         }
     },
     async created() {
